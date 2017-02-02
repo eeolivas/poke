ae378989f4ea2cf55a0f1e54d32fdafef99540ec
Efren Olivas <Efren Olivas>
2017-02-02 15:00:02 -0700

    try store pokeOne

--- scripts/scriptTest.js
+++ scripts/scriptTest.js
@@ -11,44 +11,32 @@ $(document).ready(function() {
 	$("#matchupButton").click(function(){
 		var pokeOne =(Math.floor(Math.random() * 720))+1;
 		var pokeTwo =(Math.floor(Math.random() * 720))+1;
-		
-		function getJson(pokeNum) {
-			return JSON.parse($.ajax({
-	                method: "GET",
-	                url: "https://pokeapi.co/api/v2/pokemon/" + pokeNum + "/",
-	                dataType: 'json',
-	                success: function (data) {
-	                    return data;
-	                }
-	            }).responseText);
-	        }
-		  
-		  var pokemonOne = getJson(pokeOne);
-		  console.log(pokemonOne.id);
-		
-
 
 		
-		$.ajax({
+		var pokemonOne = $.ajax({
 			method: "GET",
 			url: "https://pokeapi.co/api/v2/pokemon/" + pokeOne + "/",
 			success: function(data){
 	  			$("#pokemonOne").html(data.name);
 	  			$("#attackStat1").html("Attack: " + data.stats[4].base_stat);
 	  			$("#defenseStat1").html("Defense: " + data.stats[3].base_stat);
-	  			$("#speedStat1").html("Speed: " + data.stats[1].base_stat);	  			
+	  			$("#speedStat1").html("Speed: " + data.stats[1].base_stat);	 
+	  			return data;
 	  		}
 		});
-		$.ajax({
+		
+		
+		var pokemonTwo = JSON.parse($.ajax({
 			method: "GET",
 			url: "https://pokeapi.co/api/v2/pokemon/" + pokeTwo + "/",
 			success: function(data){
 				$("#pokemonTwo").html(data.name);
 	  			$("#attackStat2").html("Attack: " + data.stats[4].base_stat);
 	  			$("#defenseStat2").html("Defense: " + data.stats[3].base_stat);
-	  			$("#speedStat2").html("Speed: " + data.stats[1].base_stat);	  
+	  			$("#speedStat2").html("Speed: " + data.stats[1].base_stat);
+	  			return data;
 			}
-		});
+		}));
 		
 		$("#battleButton").click(function(){
 	
@@ -57,7 +45,6 @@ $(document).ready(function() {
 		});
 	
 
-		
 	});
 	
 	
