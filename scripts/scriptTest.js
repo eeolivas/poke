c63c81527039b3ee8a1125e9197ead6f13e8f3ee
Efren Olivas <Efren Olivas>
2017-02-02 13:28:31 -0700

    battlebutton added

--- scripts/scriptTest.js
+++ scripts/scriptTest.js
@@ -8,26 +8,30 @@
 
 $(document).ready(function() {
 	
-	$("#submitButton").click(function(){
+	$("#matchupButton").click(function(){
 		var pokeOne =(Math.floor(Math.random() * 720))+1;
 		var pokeTwo =(Math.floor(Math.random() * 720))+1;
 		
-	$.ajax({
-		method: "GET",
-		url: "https://pokeapi.co/api/v2/pokemon/" + pokeOne + "/",
-		success: function(data){
+		$.ajax({
+			method: "GET",
+			url: "https://pokeapi.co/api/v2/pokemon/" + pokeOne + "/",
+			success: function(data){
 	  			$("#pokemonOne").html(data.name);
 	  		}
-	});
-	$.ajax({
-		method: "GET",
-		url: "https://pokeapi.co/api/v2/pokemon/" + pokeTwo + "/",
-		success: function(data){
-			$("#pokemonTwo").html(data.name);
-		}
-	});
+		});
+		$.ajax({
+			method: "GET",
+			url: "https://pokeapi.co/api/v2/pokemon/" + pokeTwo + "/",
+			success: function(data){
+				$("#pokemonTwo").html(data.name);
+			}
+		});
+		
+		$("#battleButton").click(function(){
 	
-	
+			console.log("battle begin!");
+			
+		});
 	
 
 		
