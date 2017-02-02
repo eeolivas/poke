ba3a9bb71352c3df4d6cbaa01165fc2bb9c04980
Efren Olivas <Efren Olivas>
2017-02-02 11:30:58 -0700

    testing this again

--- scripts/scriptTest.js
+++ scripts/scriptTest.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
 		
 	$.ajax({
 		method: "GET",
-		url: "http://pokeapi.co/api/v2/pokemon/" + input + "/",
+		url: "https://pokeapi.co/api/v2/pokemon/" + input + "/",
 		success: function(data){
-	  			$("#pokemonName").html(data.name);
+	  			$("#pokemonName").html(data.id);
 	  		}
 	});
 		
