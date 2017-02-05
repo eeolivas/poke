c10f6b32c8696be93fbf7a0646a420ce1c8b2d99
eeolivas <eeolivas29@gmail.com>
2017-02-05 12:22:13 -0700

    Update scriptTest.js

--- scripts/scriptTest.js
+++ scripts/scriptTest.js
@@ -9,9 +9,23 @@ $(document).ready(function() {
 	var global ={};
 	global.poke=[];
 	global.counter=0;
+	global.pokeLevel=[0,0];
+	global.points=100;
 
+	function generatePokeLevels(){
+		var level1=0;
+		var level2=0;
+		do{
+			level1 =(Math.floor(Math.random() * 99))+1;
+			global.pokeLevel[0] = level1;
+			level2 =(Math.floor(Math.random() * 99))+1;
+			global.pokeLevel[1] = level2;
+		} while((level1>(1.5*level2))||(level2>(1.5*level1)));
+	}
+	
+	// randomly generates pokemon and stores them as objects in global.poke[]
 	function getPokemon(){
-		
+		generatePokeLevels();
 		global.counter=0;
 		console.log("beginning loop");
 		for(ii=0; ii<2; ii++){
@@ -24,28 +38,47 @@ $(document).ready(function() {
 						console.log(global.counter);
 						if(global.counter === 2){
 							$("#showMatchup").removeClass("hidden");
+							$("#battleBtn").removeClass("hidden");
 						}
 						if(global.poke.length > 1){
 							global.poke.splice(2,2);
 						}						
+					
 						console.log("length of pokemon array " + global.poke.length);
+						console.log("length of Level array " + global.pokeLevel.length);
+						
 					}
 				});
 
 			}
 		}
-
+	
+	
+		
+//		for(ii=0; ii<2; ii++){
+//			var level =(Math.floor(Math.random() * 99))+1;
+//			console.log("Level being pushed into level array: " + level);
+//			global.pokeLevel.unshift(level);
+//		}
+	//clears all fields for a new battle
 	function clearInfoFields(){
 		$("#showMatchup").addClass("hidden");
-		$("#pokemonOne").html(" ");
+		$("#battleBtn").addClass("hidden");
+		$('#poke1HpBar').attr('aria-valuenow',100).css('width',100 +'%');
+		$('#poke2HpBar').attr('aria-valuenow',100).css('width',100 +'%');
+		$('#pokePic1').addClass("hidden");
+		$('#pokePic2').addClass("hidden");
+		$('#poke1Hp').addClass("hidden");
+		$('#poke2Hp').addClass("hidden");
+		$("#pokemonOne").empty();
+		$("#pokemonLevelOne").empty();
+		$("#pokemonLevelTwo").empty();
+		$("#pokemonTwo").empty();
+		$("#heightOne").empty();
+		$("#heightTwo").empty();
+		$("#weightOne").empty();
+		$("#weightTwo").empty();
 	};
-
-//	function getPic(location, num){
-//		var pokeId = global.poke[num].id;
-//		var url = "resources/sprites/pokemon/" + pokeId + ".png"
-//		$(#location).prepend('<img id="theImg" src="'+ url +" />");
-//	}
-	
 	
 	$("#genMatchup").click(function(){
 		clearInfoFields();
@@ -54,45 +87,112 @@ $(document).ready(function() {
 		
 		
 	$("#showMatchup").click(function(){
-		console.log("button clicking now!");
+		console.log("show Matchup Now");
+		//Grabs the images and places them in the correct location on html file
+		var url1 = "<img src=resources/sprites/pokemon/" + global.poke[0].id + ".png width=\"30%\" />"
+		var url2 = "<img src=resources/sprites/pokemon/" + global.poke[1].id + ".png width=\"30%\"/>"
 		
-		
-		//fully qalified name for the local server - just use relative path when deploying to github
-	//	var url1 = "<img src=L:\resources\sprites\pokemon/" + global.poke[0].id + ".png />"
-		
-		//use this when deploying to github for sprites
-		var url1 = "<img src=resources/sprites/pokemon/" + global.poke[0].id + ".png />"
+		// displays the pokemon pic and stats
 		$('#pokePic1').empty().prepend(url1);
-		
-		
-		
-		
-//		var url1 = "resources/sprites/pokemon/" + global.poke[0].id + ".png"
-//		$('#pokePic1').prepend('<img id="theImg" src="'+ url +" />")
-//		
-		
-//		$('#test').empty().append('<img src="/static/on.png" height="64px" width="64px">');
-//		$('#pokePic1').prepend('<img id="theImg" src="theImg.png" />')
-
-			$("#pokemonOne").html(global.poke[0].name);
-			$("#attackStat1").html("Attack: " + global.poke[0].stats[4].base_stat);
-//			$("#defenseStat1").html("Defense: " + global.poke[0].stats[3].base_stat);
-//			$("#speedStat1").html("Speed: " + poke[0].stats[0].base_stat);
-			$("#pokemonTwo").html(global.poke[1].name);
-			$("#attackStat2").html("Attack: " + global.poke[1].stats[4].base_stat);
-//			$("#defenseStat2").html("Defense: " + global.poke[1].stats[3].base_stat);
-//			$("#speedStat2").html("Speed: " + global.poke[1].stats[0].base_stat);
-		
+		$('#pokePic2').empty().prepend(url2);
+		$('#pokePic1').removeClass("hidden");
+		$('#pokePic2').removeClass("hidden");
+		$('#poke1Hp').removeClass("hidden");
+		$('#poke2Hp').removeClass("hidden");
+		$("#pokemonOne").html(global.poke[0].name);
+		$("#pokemonTwo").html(global.poke[1].name);
+		$("#pokemonLevelOne").html("Level: " + global.pokeLevel[0]);
+		$("#pokemonLevelTwo").html("Level: " + global.pokeLevel[1]);
+		$("#heightOne").html("Height: " + (global.poke[0].height)/10 + "m");
+		$("#heightTwo").html("Height: " + (global.poke[1].height)/10 + "m");
+		$("#weightOne").html("Weight: " + (global.poke[0].weight)/10 + "kg");
+		$("#weightTwo").html("Weight: " + (global.poke[1].weight)/10 + "kg");	
 	});
 
 		
 		
-		$("#battleButton").click(function(){
-	
-			console.log("battle begin!");
-			
+	$("#battleButton").click(function(){
 		
+		console.log("battle begin!");
+		var turn=0; //denotes which is pokemon is to be losing its health - the array value
+		var poke0Lvl = global.pokeLevel[0];
+		var poke1Lvl = global.pokeLevel[1];		
+		var poke0Hp = (10*(global.poke[0].stats[5].base_stat))+(2.5*poke0Lvl);
+		var maxPoke0Hp = (10*(global.poke[0].stats[5].base_stat))+(2.5*poke0Lvl);
+		var poke1Hp = (10*(global.poke[1].stats[5].base_stat))+(2.5*poke1Lvl);
+		var maxPoke1Hp = (10*(global.poke[1].stats[5].base_stat))+(2.5*poke1Lvl);
+		
+		var poke0Percent = (poke0Hp/maxPoke0Hp);
+		var poke1Percent = (poke1Hp/maxPoke1Hp);
+		var poke0Atk = global.poke[0].stats[4].base_stat+(2.5*poke0Lvl);;
+		var poke1Atk = global.poke[1].stats[4].base_stat+(2.5*poke1Lvl);;
+		var poke0Dfns = global.poke[0].stats[3].base_stat+(2.5*poke0Lvl);;
+		var poke1Dfns = global.poke[1].stats[3].base_stat+(2.5*poke1Lvl);;
+		var poke0Spd = (global.poke[0].stats[0].base_stat)+(2*poke0Lvl);
+		var poke1Spd = (global.poke[1].stats[0].base_stat)+(2*poke1Lvl);
+		
+		
+		console.log(global.poke[0].name+ " Hp: " + poke0Hp);
+		console.log(global.poke[0].name + " Attack: " + poke0Atk);
+		console.log(global.poke[0].name+ " Defense: " + poke0Dfns);
+		console.log(global.poke[0].name + " Speed: " + poke0Spd);
+		console.log(global.poke[1].name+ " Hp: " + poke1Hp);
+		console.log(global.poke[1].name + " Attack: " + poke1Atk);
+		console.log(global.poke[1].name+ " Defense: " + poke1Dfns);
+		console.log(global.poke[1].name + " Speed: " + poke1Spd);
 
+
+		
+		// determines which gets to make the first move based on poke's speed stat
+		if(poke0Spd >= poke1Spd){
+			turn=1;
+		}
+		
+		var damage=0;
+		var crit=0; //variable for determining ciritcal hits
+		while((poke0Hp>0) && (poke1Hp>0) ){
+			if(turn===0){
+				damage = ((((2*poke1Lvl)+10)/200)*((poke1Atk*poke1Atk)/(1.25*poke0Dfns))+2);
+				console.log(global.poke[1].name + " attacking now");
+				crit=(Math.floor(Math.random() * 249))+1;
+				if((global.poke[1].stats[0].base_stat)>crit){
+					console.log("ciritical hit!");
+					poke0Hp = poke0Hp -(2*damage);
+				}else{
+					poke0Hp = poke0Hp -damage;
+				}
+				poke0Percent = Math.round((poke0Hp/maxPoke0Hp)*100);
+				$('#poke1HpBar').attr('aria-valuenow',poke0Percent).css('width',poke0Percent +'%');
+				console.log(global.poke[0].name + " Hp: " + poke0Hp);
+			} else{
+				damage = ((((2*poke0Lvl)+10)/200)*((poke1Atk*poke1Atk)/(1.25*poke0Dfns))+2);
+				console.log(global.poke[0].name + " attacking now");
+				crit=(Math.floor(Math.random() * 249))+1;
+				if((global.poke[0].stats[0].base_stat)>crit){
+					console.log("ciritical hit!");
+					poke1Hp = poke1Hp -(2*damage);
+				}else{
+					poke1Hp = poke1Hp -damage;
+				}
+				poke1Percent = Math.round((poke1Hp/maxPoke1Hp)*100);
+				$('#poke2HpBar').attr('aria-valuenow',poke1Percent).css('width',poke1Percent +'%');
+				console.log(global.poke[1].name + " Hp: " + poke1Hp);
+			}
+			turn++;
+			turn = turn%2;
+		}
+		
+		if(poke0Hp<=0){
+			console.log(global.poke[1].name + " WINS!");
+		}
+		if(poke1Hp<=0){
+			console.log(global.poke[0].name + " WINS!");
+		}
+		
+		$("#showMatchup").addClass("hidden");
+		$("#battleBtn").addClass("hidden");
+		
+		
 		});
 	
 	
